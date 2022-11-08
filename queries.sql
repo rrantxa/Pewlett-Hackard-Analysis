463fe0c07ce958f568492a64469a5a33d1fd476d
rrantxa <arantxa.meza@gmail.com>
2022-11-07 23:41:40 -0600

    Adding additional data.

--- queries.sql
+++ queries.sql
@@ -39,3 +39,169 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+-- Create new table for retiring employees
+DROP TABLE retirement_info;
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+-- Use different types of joins. 
+-- Inner join - joining departments and dept_namager tables
+SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info -- Left table
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Use aliases for code readability
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de 
+ON ri.emp_no = de.emp_no;
+
+SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+--Use Left Join for retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp;
+
+-- Employee count by dept. number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retirement_count
+FROM current_emp AS ce
+LEFT JOIN dept_emp AS de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM retirement_count;
+
+-- Ordering selection.
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- Create a new general employees table. 
+SELECT emp_no, 
+	first_name, 
+	last_name,
+	gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM emp_info;
+
+-- Updated and complete emp_info table.
+DROP TABLE emp_info;
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees AS e
+LEFT JOIN salaries AS s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+SELECT * FROM emp_info;
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+-- List 3: Dept. Retirees
+SELECT * FROM current_emp;
+
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+INNER JOIN dept_emp AS de
+	ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- List 4: Retirement Info for Sales Department
+SELECT * FROM retirement_info;
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	d.dept_name
+INTO sales_info
+FROM retirement_info AS ri
+INNER JOIN dept_emp AS de
+	ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+	ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales');
+
+-- List 5: Retirement Info for Mentoring Program (Sales and Dev Departments)
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	d.dept_name
+INTO sales_info
+FROM retirement_info AS ri
+INNER JOIN dept_emp AS de
+	ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+	ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
