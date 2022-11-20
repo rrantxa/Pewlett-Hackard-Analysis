d534b382c6d416e5831329cbd90fab3568beaa00
rrantxa <arantxa.meza@gmail.com>
2022-11-19 20:29:34 -0600

    Adding second deliverable.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -41,3 +41,25 @@ ORDER BY count DESC;
 
 -- Confirm that the data was retrieved.
 SELECT * FROM retiring_titles;
+
+-- SECOND DELIVERABLE
+-- Create a Mentorship Elegibility Table  that holds the employees who are eligible to participate in a mentorship program.
+SELECT DISTINCT ON (emp.emp_no) emp.emp_no,
+	emp.first_name,
+	emp.last_name,
+	emp.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title,
+-- INTO mentorship_eligibility
+FROM employees AS emp
+LEFT JOIN dept_emp AS de
+ON de.emp_no = emp.emp_no
+LEFT JOIN titles AS ti
+ON ti.emp_no = emp.emp_no
+WHERE (de.to_date = '9999-01-01')
+AND (emp.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp.emp_no;
+
+-- Confirm that the data was retrieved.
+SELECT * FROM mentorship_eligibility;
